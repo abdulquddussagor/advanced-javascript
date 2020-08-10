481fc812ac1fb0dc92766d07926779c284602e67
sagorwebdeveloper <67562467+sagorwebdeveloper@users.noreply.github.com>
2020-08-11 04:57:42 +0600

    practice done

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+//null vs undefined:
+
+//undefined case:
+// let push;
+// console.log(push);
+
+// function add(num1,num2) {
+//     console.log(num1,num2);
+// }
+// const result=add(15,15);
+// console.log(result);
+
+// function add(num1,num2) {
+//     console.log(num1,num2);
+// }
+// const result=add(15);
+// console.log(result);
+
+// const premik={name:"sagor",age:21};
+// console.log(premik.height);
+
+// let fun = undefined;
+// console.log(fun);
+
+// const array=[15,16];
+// console.log(array[11]);
