3c2c3a29fe16a37c3d6847752a1fab027ebbfb3c
sagorwebdeveloper <67562467+sagorwebdeveloper@users.noreply.github.com>
2020-08-11 03:30:05 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+//truthy:
+// []
+// {}
+// " "
+// "0"
+
+
+const age = 0;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
