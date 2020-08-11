b3a8e7ebdeaa951f2bf0f10c027b125fe788358c
sagorwebdeveloper <67562467+sagorwebdeveloper@users.noreply.github.com>
2020-08-11 22:27:51 +0600

    practice done

---
+++ map-filter.js
@@ -0,0 +1,32 @@
+const numbers=[3,4,5,6,7];
+// const output=[];
+// for(let i=0;i<numbers.length;i++){
+//     const element=numbers[i];
+//     const result=element*element;
+//     output.push(result);
+// }
+// console.log(output);
+
+
+// numbers.map(function(element,index,array){
+//     console.log(element,index,array);
+// })
+
+
+//  const result=numbers.map(function(element){
+//         return element*element;
+//      })
+//  console.log(result);    
+
+
+// const result =numbers.map(x=>x*x);
+// console.log(result);
+
+
+// const bigger = numbers.filter(x=> x>5);
+// console.log(bigger);
+
+
+
+// const bigger = numbers.find(x=> x>5);
+// console.log(bigger);
