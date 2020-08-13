5fcc04029ce2ef4532f75ed04bd2f91baea50a91
sagorwebdeveloper <67562467+sagorwebdeveloper@users.noreply.github.com>
2020-08-13 14:53:02 +0600

    practice done

---
+++ slice-splice.js
@@ -0,0 +1,10 @@
+//slice
+// const arr = [2,3,4,5,6,7,8];
+// const part = arr.slice(2,4);
+//console.log(part);
+
+//splice
+const numbs=[2,3,4,5,6,7,8];
+const part = numbs.splice(2,3,77,100,109);
+console.log(part);
+console.log(numbs);
